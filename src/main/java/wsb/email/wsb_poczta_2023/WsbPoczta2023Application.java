3c181271bbb9707817fb9f1e3db2092922514a77
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-05-13 08:15:45 +0200

    init

---
+++ src/main/java/wsb/email/wsb_poczta_2023/WsbPoczta2023Application.java
@@ -0,0 +1,13 @@
+package wsb.email.wsb_poczta_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbPoczta2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbPoczta2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/email/wsb_poczta_2023/WsbPoczta2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.email.wsb_poczta_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbPoczta2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
