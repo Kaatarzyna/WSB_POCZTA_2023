81ec81a6ea57fd435fafcbc0964eae536aac43c7
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-05-13 09:14:40 +0200

    wysyłanie mejla

--- src/main/java/wsb/email/wsb_poczta_2023/MailService.java
+++ src/main/java/wsb/email/wsb_poczta_2023/MailService.java
@@ -1,12 +1,31 @@
 package wsb.email.wsb_poczta_2023;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MailService {
 
+    final private JavaMailSender javaMailSender;
+
     void sendMail(Mail mail) {
-        // TODO
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+
+            mimeMessageHelper.setTo(mail.getRecipient());
+            mimeMessageHelper.setSubject(mail.getSubject());
+            mimeMessageHelper.setText(mail.getContent());
+
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            System.out.println("Wysyłka mejla nie powiodła się");
+        }
     }
 
 }
